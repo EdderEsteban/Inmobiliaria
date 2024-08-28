a109c0e242e0e3477a2b17d51a2c53a1a8baa4d0
Edder Santibañez <edder709@gmail.com>
2024-08-27 23:50:03 -0300

    estilo de navbar y listado de inmuebles

--- wwwroot/js/DataTable.js
+++ wwwroot/js/DataTable.js
@@ -43,3 +43,25 @@ $(document).ready(function() {
     }
     });
 });
+
+// Funcionalidad DataTable de Inquilinos
+$(document).ready(function() {
+    $('#tblInmuebles').DataTable({
+        language: {
+            "sProcessing":   "Procesando...",
+            "sLengthMenu":   "Mostrar _MENU_ entradas",
+            "sZeroRecords":  "No se encontraron resultados",
+            "sInfo":         "Mostrando entradas del _START_ al _END_ de un total de _TOTAL_ entradas",
+            "sInfoEmpty":    "Mostrando entradas del 0 al 0 de un total de 0 entradas",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ entradas)",
+            "sSearch":       "Búsqueda:",
+            "sUrl":          "",
+            "oPaginate": {
+                "sFirst":    "Primero",
+                "sPrevious": "Anterior",
+                "sNext":     "Siguiente",
+                "sLast":     "Último"
+            }
+    }
+    });
+});
