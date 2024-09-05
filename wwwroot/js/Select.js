cce321143c27b6ee4c93d577dc66cd60756d0915
Edder Santibañez <edder709@gmail.com>
2024-09-05 16:44:35 -0300

    Trabajando en Select

---
+++ wwwroot/js/Select.js
@@ -0,0 +1,11 @@
+console.log('Ingreso a Select.js');
+
+// Select2
+
+    $(document).ready(function () {
+        $('#listPropietario').select2();
+  
+        $('#listTipo').select2();
+    });
+
+
